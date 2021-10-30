f634d5ee2bd311e2a99881980101992346adb612
sajanshr <62309647+sajanshr@users.noreply.github.com>
2021-10-29 22:41:37 -0500

    First commit

---
+++ src/main/java/com/employeeManager3/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.employeeManager3.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
